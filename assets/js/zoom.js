a30c5de3cee417e58ea50ee7ba91c5ccc352c2df
jaejunlee714 <126032454+jaejunlee714@users.noreply.github.com>
2026-06-14 12:59:23 +0900

    Update zoom.js

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,6 +1,8 @@
 // Initialize medium zoom.
-$(document).ready(function () {
-  medium_zoom = mediumZoom("[data-zoomable]", {
-    background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
+$(document).ready(function() {
+  mediumZoom('[data-zoomable]', {
+    background: getComputedStyle(document.documentElement)
+        .getPropertyValue('--global-bg-color') + 'ee',  // 기존 배경색 설정 등
+    margin: 100  // 이 값을 늘릴수록 확대 시 상하좌우 여백이 커져 이미지가 작게 표시됩니다.
   });
 });
